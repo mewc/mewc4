7f8ceecbb5b50afc600824f9b7496ca399b9c051
Michael Carter <m@mewc.info>
2022-05-25 09:48:02 +1000

    add a few fixes for release

--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import Home from './view/containers/Home';
-import PastProjects from './view/containers/PastProjects';
-import {
-    BrowserRouter,
-    Switch,
-    Route
-} from "react-router-dom";
+import React from "react";
+import Home from "./view/containers/Home";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
 //https://reacttraining.com/react-router/web/example/custom-link
 function Router() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/past-projects">
-                        <PastProjects />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div >
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route component={Home} path="/">
+          <Home />
+          <Redirect from="*" to="/" />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 
 export default Router;
